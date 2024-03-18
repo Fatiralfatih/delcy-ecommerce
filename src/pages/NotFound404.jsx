fe6292325b5ad492ef7876358a3f52d38ba7fe6a
Fatiralfatih <137702546+Fatiralfatih@users.noreply.github.com>
2024-03-19 01:45:49 +0700

    (fetch) : update norFoundPage

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -5,13 +5,13 @@ const NotFound404 = () => {
     return (
         <div className="flex min-h-[60vh] justify-center items-center">
             <Card>
-                <CardContent className=" bg-violet-500/50 rounded-lg shadow-intervaless-button py-[100px] px-[150px]">
+                <CardContent className=" bg-violet-500/50 rounded-lg shadow-intervaless-button  py-[100px] px-[30px] md:px-[150px]">
                     <div className="flex justify-between gap-10 items-center">
                         <div className="flex-grow">
-                            <h1 className="text-9xl">404</h1>
+                            <h1 className=" text-4xl sm:text-9xl">404</h1>
                         </div>
                         <div className="flex flex-col gap-5">
-                            <h1 className="text-3xl">Can`t Find Page :)</h1>
+                            <h1 className="text-xl sm:text-3xl">Can`t Find Page :)</h1>
                             <Link to={'/'} className="">
                                 <p className="text-xl link-underline w-fit link-underline-black">Delcy.GG</p>
                             </Link>
